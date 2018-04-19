a7940a0f105d0e9492606b693a3916c3025c89d2
natasapan <natasa.susnjic.pantic@gmail.com>
2018-04-19 13:00:26 +0100

    TaskListAdopter

---
+++ app/src/main/java/com/deitel/study__scheduler/TaskListAdapter.java
@@ -0,0 +1,37 @@
+package com.deitel.study__scheduler;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by natas on 4/19/2018.
+ */
+
+public class TaskListAdapter extends BaseAdapter {
+
+    private final List<Task> mItems = new ArrayList<>();
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
