154394e49663644299a24b38d965e16e9ad56501
natasapan <natasa.susnjic.pantic@gmail.com>
2018-04-19 12:52:45 +0100

    Database

--- app/src/main/java/com/deitel/study__scheduler/DatabaseHelper.java
+++ app/src/main/java/com/deitel/study__scheduler/DatabaseHelper.java
@@ -9,7 +9,19 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "StudyScheduler.db";
+    private static final String DATABASE_NAME = "StudyScheduler.db";
+    private static final String TABLE_NAME_USERS = "USERS";
+    private static final String TABLE_NAME_SUBJECTS = "SUBJECTS";
+    private static final String UID = "_id";
+    private static final String USERNAME = "Username";
+    private static final String PASWORD = "Pasword";
+    private static final String NAME = "Name";
+    private static final String DATE = "Date";
+    private static final String INFORMATION= "Information";
+    private static final String SID = "_id_S";
+
+
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -17,12 +29,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE USERS (_id INTEGER PRIMARY KEY AUTOINCREMENT, Username VARCHAR(255), Pasword VARCHAR(255));");
+        db.execSQL("CREATE TABLE SUBJECTS (_id_S INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(255), Information BLOB," +
+                " FOREIGN KEY(_id_S) REFERENCES USERS(_id) );");
+
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
     }
 }
