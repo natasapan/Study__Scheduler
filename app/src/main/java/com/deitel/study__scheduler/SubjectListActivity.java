6c80ae3a239d49d63633f7fb062e84f8a1ce741c
natasapan <natasa.susnjic.pantic@gmail.com>
2018-05-16 21:45:21 +0100

    Several new activities

---
+++ app/src/main/java/com/deitel/study__scheduler/SubjectListActivity.java
@@ -0,0 +1,84 @@
+package com.deitel.study__scheduler;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by natas on 5/14/2018.
+ */
+
+public class SubjectListActivity extends AppCompatActivity{
+    String[] mSubArray;
+    ArrayList<String> mTaskArray;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subjects_list);
+
+        final ArrayAdapter<String> adapter;
+        final ArrayAdapter<String> adapt;
+
+        final ListView taskLv = (ListView) findViewById(R.id.taskListView);
+        final ListView subjectLv = (ListView) findViewById(R.id.subjectsListView);
+
+        LayoutInflater inflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup)inflater.inflate(R.layout.task_list_header,taskLv,false);
+        taskLv.addHeaderView(header);
+
+        LayoutInflater in = getLayoutInflater();
+        ViewGroup head = (ViewGroup)in.inflate(R.layout.subjects_list_header,subjectLv,false);
+        subjectLv.addHeaderView(head);
+
+        mSubArray = getResources().getStringArray(R.array.subject_list);
+        adapter = new ArrayAdapter<String>(
+                this,
+               android.R.layout.simple_list_item_1,
+                mSubArray);
+        subjectLv.setAdapter(adapter);
+
+        mTaskArray = new ArrayList<>();
+        adapt = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                mTaskArray);
+        taskLv.setAdapter(adapt);
+
+        taskLv.setFooterDividersEnabled(true);
+
+        TextView footerView;
+        footerView = (TextView) getLayoutInflater().inflate(
+                R.layout.footerview, null);
+
+        taskLv.addFooterView(footerView);
+
+        footerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), TaskListActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        subjectLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = adapter.getItem(position - 1);
+                adapt.add(str);
+
+            }
+        });
+
+    }
+}
